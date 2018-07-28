e4fb33d738463b9eb9b44b174ae3d26a4f84b643
magusmgl <magus.mgl@gmail.com>
2018-07-28 10:32:08 +0500

    js2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,29 @@ var button = document.querySelector(".write-us");
 var popup = document.querySelector(".modal-feedback");
 var close = popup.querySelector(".modal-close");
 
+
+try {
+    storage = localStorage.getItem("name");
+} catch (err) {
+    isStorageSupport = false;
+}
+
 button.addEventListener("click", function(evt) {
     evt.preventDefault();
-    popup.classList.add()
-})
+    popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function(evt){
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function(evt){
+    if(evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+        }
+    }
+});
+
