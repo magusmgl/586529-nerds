342865f7eef109ad36f45dbb41410a341f9c8881
magusmgl <magus.mgl@gmail.com>
2018-07-28 10:04:17 +0500

    js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var button = document.querySelector(".write-us");
+var popup = document.querySelector(".modal-feedback");
+var close = popup.querySelector(".modal-close");
+
+button.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.add()
+})
