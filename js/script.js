5f8a3db34736e66fa513a3fbdc19cd43043f136a
magusmgl <magus.mgl@gmail.com>
2018-07-29 17:15:11 +0500

    1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 var button = document.querySelector(".write-us");
 var popup = document.querySelector(".modal-feedback");
 var close = popup.querySelector(".modal-close");
+var username = popup.querySelector(".username");
+var email  = popup.querySelector(".email");
+var form = popup.querySelector(".feedback-form");
 
+var isStorageSupport= true;
+var storage = "";
 
 try {
-    storage = localStorage.getItem("name");
+    storage = localStorage.getItem("username");
 } catch (err) {
     isStorageSupport = false;
 }
@@ -12,7 +17,22 @@ try {
 button.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
+    if (storage) {
+        username.value = storage;
+        email.focus();
+    } else {
+        username.focus();
+    }
 });
+
+form.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    if (!username.value) {
+        evt.preventDefault()
+    } else if (isStorageSupport) {
+        localStorage.setItem("username", username.value);
+    }
+})
 
 close.addEventListener("click", function(evt){
     evt.preventDefault();
